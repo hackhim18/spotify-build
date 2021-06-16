1c077858dba0bfa59cd7c30bc5e47c2a19c77928
Himanshu Tripathi <triphimanshu18@gmail.com>
2021-06-16 08:22:14 +0530

    v9 commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,17 +1,27 @@
-import React from 'react'  
-import "./Sidebar.css"
+import React from "react";
+import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+import { getTokenFromResponse } from "./spotify";
+//import { useStateValue } from "./StateProvider";
 
 function Sidebar() {
-    return (
-        <div className="sidebar">
-            <img
+   
+  return (
+    <div className="sidebar">
+      <img
         className="sidebar__logo"
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt=""
       />
-           <h1>I am the Sidebar</h1> 
-        </div>
-    )
+      <SidebarOption Icon={HomeIcon} option="Home" />
+      <SidebarOption Icon={SearchIcon} option="Search" />
+      <SidebarOption Icon={LibraryMusicIcon} option="Your Library" />
+      
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
